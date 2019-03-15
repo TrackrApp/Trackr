cae692d9f442f9fda68a82c099d98f35e5bc52c9
Dick Wolff <dckwlff@gmail.com>
2019-03-15 16:40:37 +0100

    Menu is now also i18n'd.

--- Src/Trackr.Website/src/app/shared/sidebar/sidebar.module.ts
+++ Src/Trackr.Website/src/app/shared/sidebar/sidebar.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { MatListModule } from "@angular/material";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared.module";
 import { AppVersionService } from "../version/version.service";
 import { SidebarComponent } from "./sidebar.component";
 
@@ -12,6 +13,7 @@ import { SidebarComponent } from "./sidebar.component";
   imports: [
     BrowserModule,
     RouterModule,
+    SharedModule,
     MatListModule
   ],
   providers: [
